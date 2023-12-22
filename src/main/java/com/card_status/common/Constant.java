013078b984d371e5fc0d44e8adfe2808b5e10b82
biswaranjanbehera <bmuna314@gmail.com>
2023-12-22 18:27:09 +0530

    dockerfile added

--- src/main/java/com/card_status/common/Constant.java
+++ src/main/java/com/card_status/common/Constant.java
@@ -1,10 +1,18 @@
 package com.card_status.common;
 
 public class Constant {
-    public static String PICKUP_PATH = "E:/Card_status/card-status/csv_file/Pickup_Sample_card.csv";
-    public static String DELIVERED_PATH = "E:/Card_status/card-status/csv_file/Delivered_Sample_card.csv";
-    public static String DELIVER_EXCEPTION_PATH = "E:/Card_status/card-status/csv_file/Delivery_exception_Sample_card.csv";
-    public static String RETURNED_PATH = "E:/Card_status/card-status/csv_file/Returned_Sample_card.csv";
+    //public static String PICKUP_PATH = "E:/Card_status/card-status/csv_file/Pickup_Sample_card.csv";
+    public static String PICKUP_PATH = "/home/biswa/Documents/my_project/card-status/csv_file/Pickup_Sample_card.csv";
+
+    //public static String DELIVERED_PATH = "E:/Card_status/card-status/csv_file/Delivered_Sample_card.csv";
+    public static String DELIVERED_PATH = "/home/biswa/Documents/my_project/card-status/csv_file/Delivered_Sample_card.csv";
+
+    //public static String DELIVER_EXCEPTION_PATH = "E:/Card_status/card-status/csv_file/Delivery_exception_Sample_card.csv";
+    public static String DELIVER_EXCEPTION_PATH = "/home/biswa/Documents/my_project/card-status/csv_file/Delivery_exception_Sample_card.csv";
+
+    //public static String RETURNED_PATH = "E:/Card_status/card-status/csv_file/Returned_Sample_card.csv";
+    public static String RETURNED_PATH = "/home/biswa/Documents/my_project/card-status/csv_file/Returned_Sample_card.csv";
+
 }
 
  /*String pickupPath = "E:/Project/Card_status/Getting-the-card-status/Getting-the-card-status/csv_file/Pickup_Sample_card.csv";
